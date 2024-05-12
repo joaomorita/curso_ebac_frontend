8f9c0153c9c7d0262d8c320e044c25a57bc79551
Morita <joao@teste.com>
2024-05-12 15:53:11 -0300

    Exercicio plugins

---
+++ exercicio-plugins/js/main.js
@@ -0,0 +1,11 @@
+$('document').ready(function(){
+
+    // Mascára para o CPF
+    $('#cpf').mask('000.000.000-00')
+
+    // Mascára para o telefone
+    $('#telefone').mask('(00) 00000-0000')
+
+    // Mascára para CEP:
+    $('#cep').mask('00000-000')
+})
