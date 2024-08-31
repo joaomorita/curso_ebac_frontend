49da8b1c4a64941be7828368bc857cbe7a3d25ae
Morita <joao@teste.com>
2024-08-31 18:39:16 -0300

    exercicio typescript

---
+++ TYPESCRIPT/exercicio_ts/exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+function somaNumero(a, b) {
+    return a + b;
+}
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
---
+++ TYPESCRIPT/exercicio_ts/exercicio.ts
@@ -0,0 +1,8 @@
+function somaNumero(a: number, b: number){
+    return a + b;
+}
+
+
+function saudacao(nome: string){
+    return `Olá ${nome}`
+}
